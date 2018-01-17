e71bab7f708d58f6d0b1963ab0b6f9cba4bb40c4
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-01-17 19:17:59 +0530

    Update graphFun.c

--- graphFun.c
+++ graphFun.c
@@ -9,19 +9,16 @@ void dsplG(),clrG(),grph(),putPoint(int,int,char),fill(int,int,char);
 double mod(double),gx2x(int,double);
 double rectSin(double,double),sinc(double);
 int x2gx(double,double),y2gy(double,double);
-double f(double x)
-{
-	return sin(x);
+double f(double x){
+	return rectSin(x,1);
 }
-int main()
-{
+int main(){
 	clrG();
 	grph();
 	dsplG();
 	return 0;
 }
-void grph()
-{
+void grph(){
 	double lr,ur,mxx,mxy,fx;
 	int a,b,x,y,i;
 	printf("Lower Range= ");
@@ -34,40 +31,33 @@ void grph()
 	printf("%lf %lf %lf\n",lr,ur,mxx);
 	a=x2gx(lr,mxx),b=x2gx(ur,mxx);
 	mxy=f(gx2x(a,mxx));
-	for(x=a;x<=b;x++)
-	{
+	for(x=a;x<=b;x++){
 		fx=f(gx2x(x,mxx));
 		//printf("x=%lf\tgx=%d\tf(x)=%lf\n",gx2x(x,mxx),x,fx);
 		if(fx>mxy)
 			mxy=fx;
 	}
 	printf("mxy=%lf\n",mxy);
-	for(x=a;x<=b;x++)
-	{
+	for(x=a;x<=b;x++){
 		y=y2gy(f(gx2x(x,mxx)),mxy);
 		//printf("f(%d) = %d\n",x,y);
 		putPoint(x,y,'*');
-		fill(x,y,':');		//to get the line from x-axis to f(x)
+		fill(x,y,':');
 	}
 }
-void dsplG()
-{
+void dsplG(){
 	int i,j;
-	for(i=0;i<R;i++)
-	{
+	for(i=0;i<R;i++){
 		for(j=0;j<C;j++)
 			printf("%c",G[i][j]);
 		printf("\n");
 	}
 	printf("\n");
 }
-void clrG()
-{
+void clrG(){
 	int i,j;
-	for(i=0;i<R;i++)
-	{
-		for(j=0;j<C;j++)
-		{
+	for(i=0;i<R;i++){
+		for(j=0;j<C;j++){
 			if(i==N && j==2*N)
 				G[i][j]='o';
 			else if(j==2*N)
@@ -79,30 +69,24 @@ void clrG()
 		}
 	}
 }
-double mod(double x)
-{
+double mod(double x){
 	return (x<0)?-x:x;
 }
-int x2gx(double x,double mxx)
-{
+int x2gx(double x,double mxx){
 	int gx=(2*N)*x/mxx;
 	return gx;
 }
-double gx2x(int x,double mxx)
-{
+double gx2x(int x,double mxx){
 	return mxx*x/(2*N);
 }
-int y2gy(double y,double mxy)
-{
+int y2gy(double y,double mxy){
 	int gy=N*y/mxy;
 	return gy;
 }
-void putPoint(int x,int y,char c)
-{
+void putPoint(int x,int y,char c){
 	G[N-y][2*N+x]=c;
 }
-void fill(int x,int y,char c)
-{
+void fill(int x,int y,char c){
 	int i;
 	if(y>0)
 		for(i=1;i<y;i++)
@@ -112,15 +96,13 @@ void fill(int x,int y,char c)
 			putPoint(x,i,c);
 		
 }
-double rectSin(double x,double f)
-{
+double rectSin(double x,double f){
 	int i;
 	double s=0;
 	for(i=1;i<=5;i++)
 		s+=sin(2*PI*f*x*(2*i-1))/(2*i-1);
 	return s*4/PI;
 }
-double sinc(double x)
-{
+double sinc(double x){
 	return (x==0)?1:sin(x)/x;
 }
