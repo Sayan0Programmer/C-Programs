53dce9411c1821d3c4a04e02df19d7a95a04dd4d
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-06-07 20:56:59 +0530

    Update txt2bin.c

--- String-Converter/char2bin/txt2bin.c
+++ String-Converter/char2bin/txt2bin.c
@@ -1,13 +1,13 @@
 //$ gcc -o T txt2bin.c[ENTER]
 //$ ./T <message string>[ENTER]
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 void printC2B(char);
 int main(int argc, char *argv[]){
 	int i, j, n;
 	char *A;
-	for(i=1; i<argc; i++){
+	for(i = 1; i < argc; i ++){
 		A = argv[i], n = strlen(A);
 		for(j = 0; j < n; printC2B(A[j++]));
 		printf(" ");
