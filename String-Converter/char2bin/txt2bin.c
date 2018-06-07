9987177c38780ca99949130e9a60d364b79acf0a
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-06-07 20:59:33 +0530

    Edit
    Removing unnecessary characters

--- String-Converter/char2bin/txt2bin.c
+++ String-Converter/char2bin/txt2bin.c
@@ -1,15 +1,15 @@
 //$ gcc -o T txt2bin.c[ENTER]
 //$ ./T <message string>[ENTER]
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 void printC2B(char);
 int main(int argc, char *argv[]){
 	int i, j, n;
 	char *A;
-	for(i = 1; i < argc; i ++){
-		A = argv[i], n = strlen(A);
-		for(j = 0; j < n; printC2B(A[j++]));
+	for(i=1; i<argc; i++){
+		A=argv[i], n=strlen(A);
+		for(j=0; j<n; printC2B(A[j++]));
 		printf(" ");
 	}
 	printf("\n");
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]){
 }
 void printC2B(char c){
 	printf("<");
-	for(char i = 7; i > -1; i--)
-		printf("%d", (c >> i) & 0x01);
+	for(char i=7; i>-1; i--)
+		printf("%d", (c>>i) & 0x01);
 	printf(">");
 }
